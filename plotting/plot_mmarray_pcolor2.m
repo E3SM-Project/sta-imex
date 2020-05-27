94b64014177c5068a120ecb32fbed6a9f75fb510
onguba <oksana.guba@gmail.com>
2020-05-27 13:15:22 -0600

    new label for x axis

--- plotting/plot_mmarray_pcolor2.m
+++ plotting/plot_mmarray_pcolor2.m
@@ -4,16 +4,22 @@ plot_prepare1;
 courantplot=0;
 
 ressetdx=EC/12./resset/1000;
-pcolor(ressetdx,DTset,mm2);
+%making this 2dx wave horizontal axis
+%pcolor(ressetdx,DTset,mm2);
+pcolor(2*ressetdx,DTset,mm2);
 %shading faceted;
 shading flat;
 
 plot_prepare2;
 
 ylabel('$\Delta t$ (sec)','Interpreter','latex')
-xlabel('$\Delta x$ (km)','Interpreter','latex')
+%xlabel('$\Delta x$ (km)','Interpreter','latex')
+%xlabel('$2\Delta x = 2\pi/k_x$ (km)','Interpreter','latex')
+xlabel('$2\pi/k_x$ (km)','Interpreter','latex')
 
-set(gca,'XTick',[1,2,4,10,25,50,100])
+
+%set(gca,'XTick',[1,2,4,10,25,50,100])
+set(gca,'XTick',2*[1,2,4,10,25,50,100])
 set(gca,'YTick',[1,2,4,10,20,50,100,200,300])
 
 
